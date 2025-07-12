ecfb1c529857d7658dc1f863070a64c8b7a558a7
MD Iftekhar Hossain <150613627+efti-dot@users.noreply.github.com>
2025-07-13 01:41:38 +0600

    Update Interval 2.py

--- Interval 2.py
+++ Interval 2.py
@@ -1,1 +1,13 @@
 N = int(input())
+In = 0
+Out = 0
+for i in range(1, N+1):
+    A = int(input())
+    if A >= 10 and A <= 20:
+        In += 1
+    else:
+        Out += 1
+
+print(f"{In} in")
+print(f"{Out} out")
+
