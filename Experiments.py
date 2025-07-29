50f33e7216cd2e668c1c60ea349b7fc099c9cfa8
MD Iftekhar Hossain <150613627+efti-dot@users.noreply.github.com>
2025-07-30 00:01:16 +0600

    Update Experiments.py

--- Experiments.py
+++ Experiments.py
@@ -1,9 +1,19 @@
 N = int(input())
-s = 0
+x = 0
+C, R, S = 0
 for i in range(N):
     a, b = input().split()
     a = int(a)
     if 1<= a <=15:
-        s = s + a
+        x = x + a
+        if b == 'C':
+            C += a
+        elif b == 'R':
+            R += a
+        elif b == 'S':
+            S += a
 
-print(s)
+print(f"Total: {x} cobaias")
+print(f"Total de coelhos: {C}")
+print(f"Total de ratos: {R}")
+print(f"Total de sapos: {S}")
