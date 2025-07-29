b29c597c5883d297831696d6a17e7d1322aeaaf6
MD Iftekhar Hossain <150613627+efti-dot@users.noreply.github.com>
2025-07-30 00:51:33 +0600

    Update Experiments.py

--- Experiments.py
+++ Experiments.py
@@ -1,6 +1,8 @@
 N = int(input())
 x = 0
-C, R, S = 0
+C = 0
+R = 0
+S = 0
 for i in range(N):
     a, b = input().split()
     a = int(a)
@@ -17,3 +19,6 @@ print(f"Total: {x} cobaias")
 print(f"Total de coelhos: {C}")
 print(f"Total de ratos: {R}")
 print(f"Total de sapos: {S}")
+print(f"Percentual de coelhos: {C*100/x:.2f} %")
+print(f"Percentual de ratos: {R*100/x:.2f} %")
+print(f"Percentual de sapos: {S*100/x:.2f} %")
