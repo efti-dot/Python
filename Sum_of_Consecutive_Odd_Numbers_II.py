8b25c177e5c763531d8edadfe45a909939188ab6
MD Iftekhar Hossain <150613627+efti-dot@users.noreply.github.com>
2025-09-01 23:58:10 +0600

    Update Sum_of_Consecutive_Odd_Numbers_II.py

--- Sum_of_Consecutive_Odd_Numbers_II.py
+++ Sum_of_Consecutive_Odd_Numbers_II.py
@@ -3,7 +3,7 @@ sum = 0
 for i in range(N):
     a = input().split(" ")
     X, Y = int(a[0]), int(a[1])
-    for j in range(X, Y):
+    for j in range(X, Y+1):
         if j%2 != 0:
             sum += j
     
