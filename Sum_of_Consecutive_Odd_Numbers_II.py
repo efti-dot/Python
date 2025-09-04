014386d24c5cff228fba59b64feb7c076b94ca31
MD Iftekhar Hossain <150613627+efti-dot@users.noreply.github.com>
2025-09-05 01:43:57 +0600

    Update Sum_of_Consecutive_Odd_Numbers_II.py

--- Sum_of_Consecutive_Odd_Numbers_II.py
+++ Sum_of_Consecutive_Odd_Numbers_II.py
@@ -3,9 +3,8 @@ sum = 0
 for i in range(N):
     a = input().split(" ")
     X, Y = int(a[0]), int(a[1])
-    for j in range(X+1, Y):
-        print(j)
+    for j in range(min(X,Y)+1, max(X,Y)):
         if j%2 != 0:
-            print(j)
             sum += j
     print(sum)
+    sum = 0
