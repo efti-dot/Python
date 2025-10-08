8797129e3d3e5082c7770840bf471436c4940725
MD Iftekhar Hossain <150613627+efti-dot@users.noreply.github.com>
2025-10-09 01:49:29 +0600

    Update sequence_of_numbers&sum.py

--- sequence_of_numbers&sum.py
+++ sequence_of_numbers&sum.py
@@ -3,3 +3,13 @@ a = []
 while(1):
     a = input().split(" ")
     M, N = int(a[0]), int(a[1])
+
+    while(1):
+        if M == 0 or N == 0:
+            break
+        else:
+            if M>N:
+                M, N = N, M
+            sum = 0
+            for i in range (M, N+1):
+                print(i)
