68707a5177511b5f9d1573d1b7bed3217cd174e3
efti-dot <eftih31@gmail.com>
2026-06-07 22:35:50 +0600

    Create Quadrant.py

---
+++ Quadrant.py
@@ -0,0 +1,13 @@
+while True:
+    a = input().split(" ")
+    x, y = int(a[0]), int(a[1])
+    if x==0 or y ==0:
+        break
+    elif x>0 & y>0:
+        print("primeiro")
+    elif x>0 & y<0:
+        print("quarto")
+    elif x<0 & y<0:
+        print("terceiro")
+    else:
+        print("segundo")
