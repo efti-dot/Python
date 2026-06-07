73601cfcc442e6cc4843c77234d90e508b860b8a
efti-dot <eftih31@gmail.com>
2026-06-07 22:47:27 +0600

    debug

--- Quadrant.py
+++ Quadrant.py
@@ -1,14 +1,13 @@
 while True:
     a = input().split(" ")
     x, y = int(a[0]), int(a[1])
-    print(x,y)
     if x==0 or y ==0:
         break
-    elif x>0 & y>0:
+    elif x>0 and y>0:
         print("primeiro")
-    elif x>0 & y<0:
+    elif x>0 and y<0:
         print("quarto")
-    elif x<0 & y<0:
+    elif x<0 and y<0:
         print("terceiro")
     else:
         print("segundo")
