781cb89ddb6e910d6af876408d128150b0ae8941
efti-dot <eftih31@gmail.com>
2026-06-11 00:09:14 +0600

    typecast to float and find avg

--- Score_Validation.py
+++ Score_Validation.py
@@ -1,8 +1,12 @@
 vaild_count = 0
+score = []
 while vaild_count<2:
-    i = input()
-    if 0<i or i>10:
+    i = float(input())
+    if i<0 or i>10:
         print("nota invalida")
     else:
         vaild_count += 1
-        
+        score.append(i)
+
+avg = (score[0]+score[1])/2
+print(f"media = {avg:.2f}")
