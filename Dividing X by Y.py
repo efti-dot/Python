96a89ec068054793844e6b9fd1a6cf14caa1dfda
efti-dot <eftih31@gmail.com>
2026-06-10 23:36:34 +0600

    Update Dividing X by Y.py

--- Dividing X by Y.py
+++ Dividing X by Y.py
@@ -1,5 +1,9 @@
-N = input()
+N = int(input())
 for i in range(N):
     Pair = input().split(" ")
     X, Y = int(Pair[0]), int(Pair[1])
-    
+    if Y==0:
+        print("divisao impossivel")
+    else:
+        result = X/Y
+        print(f"{result:.1f}")
