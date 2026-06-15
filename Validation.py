5de73e1d3a3e4fd79c41ef9bfc375bc6d1a5adbc
efti-dot <eftih31@gmail.com>
2026-06-15 23:42:11 +0600

    Update Validation.py

--- Validation.py
+++ Validation.py
@@ -1,9 +1,11 @@
-X = 0
+count = 0
 l = []
 while X==2:
     a = input()
     if a>0 or a<10:
         l.append(a)
-        X += 1
+        count += 1
     else:
         print("nota invalida")
+
+print(f"media =" {((int(l[0])+int(l[1]))/2)})
