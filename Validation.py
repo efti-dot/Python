fb075418b5e10cb2d4d46d4566153b4becb6097d
efti-dot <eftih31@gmail.com>
2026-06-16 01:13:35 +0600

    Update Validation.py

--- Validation.py
+++ Validation.py
@@ -1,19 +1,18 @@
 count = 0
 l = []
 while True:
-    a = input()
-    if a>0 or a<10:
+    a = float(input())
+    if a>=0 or a<=10:
         count += 1
         l.append(a)
         if count==2:
+            print(f"media = {((int(l[0])+int(l[1]))/2)}")
             print("novo calculo (1-sim 2-nao)")
             X = int(input())
             if X == 1:
                 continue
-            else:
+            elif X == 2:
                 break
     else:
         print("nota invalida")
-
-print(f"media = {((int(l[0])+int(l[1]))/2)}")
 
