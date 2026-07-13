d2f61bfc503cbff11fe5fdaed67f701ef29d8a60
efti-dot <eftih31@gmail.com>
2026-07-13 23:25:59 +0600

    Update Validation.py

--- Validation.py
+++ Validation.py
@@ -9,7 +9,7 @@ while True:
             print(f"media = {((int(l[0])+int(l[1]))/2)}")
             while True:
                 print("novo calculo (1-sim 2-nao)")
-                print("okay")
+                print("okay!")
                 X = int(input())
                 if X == 1:
                     count = 0
